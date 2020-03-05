3a0883b6ce793f9fe98c3dfb7a6f28f81a748c7a
JohamMedina <joham.nba@gmail.com>
2020-03-05 18:03:32 -0500

    add solution basketballoneonone

---
+++ apaxiaaans.py
@@ -0,0 +1,11 @@
+output=input().lower()
+cont=0
+if len(output) == 0:
+	print(output)
+else:
+	while cont < len(output)-1 :
+		if output[cont] == output[cont+1]:
+			output=output.replace(output[cont]+output[cont+1],output[cont])
+			cont -=1
+		cont += 1
+	print(output)	

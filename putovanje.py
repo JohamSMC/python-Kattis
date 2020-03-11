b45c31cc45beb4f7754b8cc28efff15d464578fa
JohamMedina <joham.nba@gmail.com>
2020-03-10 22:16:05 -0500

    add solution puntovanje

---
+++ putovanje.py
@@ -0,0 +1,18 @@
+n, c = list(map(int, input().split(" ")))
+fruitWeight = list(map(int, input().split(" ")))
+output = 0
+auxOutput = []
+
+for i in range(len(fruitWeight)-1):
+    if len(fruitWeight)-i <= output:
+        break
+    auxOutput = []
+    for j in range(i,len(fruitWeight)):
+        if sum(auxOutput) == c:
+            break
+        if (fruitWeight[j]+sum(auxOutput) <= c):
+            auxOutput.append(fruitWeight[j])
+        # print("J =",j,end = "\t")
+    if  len(auxOutput) > output:
+        output = len(auxOutput)
+print(output)

5f0905c85e3c9fd3d234da659b652282107cc817
Joham Medina <joham.nba@gmail.com>
2020-03-16 23:22:38 -0500

    add solution oddmanout

---
+++ oddmanout.py
@@ -0,0 +1,13 @@
+numberCases = int(input())
+for case in range(numberCases):	
+	numberGuests = int(input())
+	invitationCodes= list(input().split(" "))
+	auxInvitationCodes = " ".join(invitationCodes)+" "	
+	for i in range((len(invitationCodes)//2)+1):		
+		if auxInvitationCodes.count(invitationCodes[i]+" ") == 1:
+		    print("Case #",(case+1),": ",invitationCodes[i],sep="")
+		    break
+		if auxInvitationCodes.count(invitationCodes[len(invitationCodes)-(i+1)]+" ") == 1:
+			print("Case #",(case+1),": ",invitationCodes[len(invitationCodes)-(i+1)],sep="")
+
+			
